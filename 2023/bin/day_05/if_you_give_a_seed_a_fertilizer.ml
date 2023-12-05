6c877eb03998cc4be6dd5f9a4c41e3a9b41ddc94
Stanislau Tur <stanislau.tur@gmail.com>
2023-12-05 18:48:19 +0100

    Day 5, part 2

--- 2023/bin/day_05/if_you_give_a_seed_a_fertilizer.ml
+++ 2023/bin/day_05/if_you_give_a_seed_a_fertilizer.ml
@@ -10,7 +10,7 @@ let convert seeds map =
       in
       match n with Some v -> v | None -> seed)
 
-let parse_range line =
+let parse_mapping line =
   Stdlib.Scanf.sscanf line "%d %d %d" (fun d s l -> (s, d, l))
 
 let parse_seeds line =
@@ -25,19 +25,75 @@ let parse_maps lines =
     | line :: lines when String.is_suffix line ~suffix:"map:" ->
         aux lines curr_map maps
     | line :: lines ->
-        let range = parse_range line in
+        let range = parse_mapping line in
         aux lines (range :: curr_map) maps
   in
   aux lines [] [] |> List.rev
 
+let parse_seed_ranges line =
+  let nums = parse_seeds line in
+  let rec aux nums ranges =
+    match nums with
+    | s :: l :: nums -> aux nums ((s, s + l - 1) :: ranges)
+    | [] | _ -> ranges
+  in
+  aux nums []
+
+let map_range range map =
+  let rec aux ranges_to_check mapped_ranges left_mappings =
+    match ranges_to_check with
+    | [] -> mapped_ranges
+    | (low, high) :: ranges_tl -> (
+        match left_mappings with
+        | [] -> mapped_ranges @ ranges_to_check
+        | (src, dest, len) :: tl ->
+            let m_low = src in
+            let m_high = src + len - 1 in
+            let to_dest n = n - src + dest in
+
+            let to_check, mapped =
+              if high < m_low || low > m_high then
+                (* no overlap *)
+                ([ (low, high) ], [])
+              else if m_low <= low then
+                if high <= m_high then
+                  (* mapping contains range *)
+                  ([], [ (to_dest low, to_dest high) ])
+                else
+                  (* range end is after mapping end  *)
+                  ([ (m_high + 1, high) ], [ (to_dest low, to_dest m_high) ])
+              else if low < m_low && high <= m_high then
+                (* range start is before mapping start *)
+                ( [ (low, m_low - 1); (high, m_high) ],
+                  [ (to_dest m_low, to_dest m_high) ] )
+              else ([ (low, m_low - 1) ], [ (to_dest m_low, to_dest high) ])
+            in
+            aux (to_check @ ranges_tl) (mapped @ mapped_ranges) tl)
+  in
+  aux [ range ] [] map
+
 let () =
   let lines = Utils.read_lines () in
-  let seeds = lines |> List.hd_exn |> parse_seeds in
   let maps = List.drop lines 2 |> parse_maps in
 
   let part1 =
+    let seeds = lines |> List.hd_exn |> parse_seeds in
     List.fold maps ~init:seeds ~f:convert
     |> List.min_elt ~compare |> Option.value_exn
   in
 
-  Utils.print_result (Int.to_string part1) ""
+  let part2 =
+    let ranges = lines |> List.hd_exn |> parse_seed_ranges in
+
+    let planted_ranged =
+      List.fold maps ~init:ranges ~f:(fun ranges map ->
+          List.fold ranges ~init:[] ~f:(fun acc range ->
+              acc @ map_range range map))
+    in
+
+    planted_ranged
+    |> List.min_elt ~compare:(fun (a, _) (b, _) -> a - b)
+    |> Option.value_exn |> fst
+  in
+
+  Utils.print_result (Int.to_string part1) (Int.to_string part2)
