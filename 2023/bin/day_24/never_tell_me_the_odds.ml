cf6354a1e9c840f7c3ebebd1eac02b18c71c108f
Stanislau Tur <stanislau.tur@gmail.com>
2023-12-26 19:26:44 +0100

    Day 24, part 2 (not finished)

--- 2023/bin/day_24/never_tell_me_the_odds.ml
+++ 2023/bin/day_24/never_tell_me_the_odds.ml
@@ -1,39 +1,73 @@
 open Base
-open Float
 
-type point_3d = { x : float; y : float; z : float }
-[@@deriving sexp, compare, equal]
+type point_3d = { x : int; y : int; z : int } [@@deriving sexp, compare, equal]
 
 type t = point_3d * point_3d [@@deriving sexp, compare, equal]
-
-let print t =
-  let { x; y; z }, { x = vx; y = vy; z = vz } = t in
-  Stdio.printf "%f, %f, %f, @ %f, %f, %f\n" x y z vx vy vz
 
 let parse_line line =
   String.split_on_chars line ~on:[ '@'; ',' ]
   |> List.filter ~f:(Fn.non String.is_empty)
-  |> List.map ~f:String.strip
-  |> List.map ~f:Float.of_string
+  |> List.map ~f:String.strip |> List.map ~f:Int.of_string
   |> function
   | [ x; y; z; vx; vy; vz ] -> ({ x; y; z }, { x = vx; y = vy; z = vz })
   | _ -> failwith "invalid input"
 
-let det a b c d = (a * d) - (b * c)
+let get_cofactor m row col =
+  Array.filteri m ~f:(fun i _ -> i <> row)
+  |> Array.transpose_exn
+  |> Array.filteri ~f:(fun i _ -> i <> col)
+  |> Array.transpose_exn
 
-let cramers_rule a1 b1 s1 a2 b2 s2 =
-  let d = det a1 b1 a2 b2 in
-  let dx = det s1 b1 s2 b2 in
-  let dy = det a1 s1 a2 s2 in
+let rec determinant_of_matrix m =
+  if Array.length m = 1 then m.(0).(0)
+  else
+    Array.foldi m ~init:0 ~f:(fun i d _ ->
+        let sign = if i % 2 = 0 then 1 else -1 in
+        let cofactor = get_cofactor m 0 i in
+        d + (sign * m.(0).(i) * determinant_of_matrix cofactor))
 
-  if d = 0. then None else Some (dx /. d, dy /. d)
+let cramer matrix =
+  let n = Array.length matrix.(0) in
+
+  let di m idx =
+    let m = Array.to_list m |> List.map ~f:Array.to_list in
+    let m =
+      List.map m ~f:(fun l ->
+          let last = List.last_exn l in
+          List.drop_last_exn l
+          |> List.mapi ~f:(fun i x -> if i = idx then last else x))
+    in
+    m |> List.map ~f:Array.of_list |> Array.of_list
+  in
+
+  let d = Int.to_float @@ determinant_of_matrix (di matrix (n - 1)) in
+
+  if Float.(d = 0.) then None
+  else
+    List.range 0 (n - 1)
+    |> List.map ~f:(fun i ->
+           Int.to_float (determinant_of_matrix (di matrix i)) /. d)
+    |> Option.some
 
 let find_intersection_point_2d (p1, v1) (p2, v2) =
-  match cramers_rule v1.x (-v2.x) (p2.x - p1.x) v1.y (-v2.y) (p2.y - p1.y) with
-  | None -> None
-  | Some (t, s) ->
-      if t < 0. || s < 0. then None
-      else Some { x = p1.x +. (t *. v1.x); y = p1.y +. (t *. v1.y); z = 0. }
+  let a1 = v1.x in
+  let b1 = -v2.x in
+  let s1 = p2.x - p1.x in
+
+  let a2 = v1.y in
+  let b2 = -v2.y in
+  let s2 = p2.y - p1.y in
+
+  match cramer [| [| a1; b1; s1 |]; [| a2; b2; s2 |] |] with
+  | Some [ t; s ] ->
+      if Float.(t < 0. || s < 0.) then None
+      else
+        let x = Int.to_float p1.x in
+        let y = Int.to_float p1.y in
+        let vx = Int.to_float v1.x in
+        let vy = Int.to_float v1.y in
+        Some (x +. (t *. vx), y +. (t *. vy))
+  | _ -> None
 
 let build_pairs l =
   let rec aux l acc =
@@ -45,91 +79,47 @@ let build_pairs l =
   in
   aux l []
 
-let proportions a b = (a.x /. b.x, a.y /. b.y, a.z /. b.z)
-
-let distance a b =
-  let x = b.x -. a.x in
-  let y = b.y -. a.y in
-  let z = b.z -. a.z in
-  sqrt ((x *. x) +. (y *. y) +. (z *. z))
-
-let are_on_same_line a b c =
-  let abx = b.x -. a.x in
-  let aby = b.y -. a.y in
-  let abz = b.z -. a.z in
-  let bcx = c.x -. b.x in
-  let bcy = c.y -. b.y in
-  let bcz = c.z -. b.z in
-
-  abx /. bcx = aby /. bcy && abx /. bcx = abz /. bcz
-
-let all_points_on_same_line points =
-  let rec aux points =
-    match points with
-    | a :: b :: c :: tl -> are_on_same_line a b c && aux (b :: c :: tl)
-    | _ -> true
-  in
-  aux points
-
-let find_pos_at t ~h:(p, v) =
-  let x = p.x +. (t *. v.x) in
-  let y = p.y +. (t *. v.y) in
-  let z = p.z +. (t *. v.z) in
-  { x; y; z }
-
-let solve_1 h0 h1 h2 =
-  try
-    Some
-      (for t = 1 to 1000 do
-         let p0 = find_pos_at (Int.to_float t) ~h:h0 in
-
-         for t = 1 to 1000 do
-           let p1 = find_pos_at (Int.to_float t) ~h:h1 in
-
-           for t = 1 to 1000 do
-             let p2 = find_pos_at (Int.to_float t) ~h:h2 in
-
-             if are_on_same_line p0 p1 p2 then (
-               Stdio.printf "%f %f %f\n" p0.x p0.y p0.z;
-               Stdio.printf "%f %f %f\n" p1.x p1.y p1.z;
-               Stdio.printf "%f %f %f\n" p2.x p2.y p2.z;
-               failwith "found")
-           done
-         done
-       done)
-  with _ -> None
+let make_equation (p0, v0) (p1, v1) =
+  let a = v0.y - v1.y in
+  let b = v0.x - v1.x in
+  let c = p1.y - p0.y in
+  let d = p0.x - p1.x in
+  let s = (p0.x * v0.x) - (p0.y * v0.x) + (p1.y * v1.x) - (p1.x * v1.y) in
+  (* x; y; vx; vy; *)
+  [| a; b; c; d; s |]
 
 let () =
   let hails = Utils.read_lines () |> List.map ~f:parse_line in
 
-  (* let min = 200000000000000. in
-     let max = 400000000000000. in *)
-  let min = 7. in
-  let max = 27. in
+  (* let left_bound = 200000000000000. in
+     let right_bound = 400000000000000. in *)
+  let left_bound = 7. in
+  let right_bound = 27. in
 
   let part1 =
     let pairs = build_pairs hails in
     let intersections =
       List.filter_map pairs ~f:(fun (a, b) -> find_intersection_point_2d a b)
-      |> List.filter ~f:(fun { x; y; _ } ->
-             x >= min && x <= max && y >= min && y <= max)
+      |> List.filter ~f:(fun (x, y) ->
+             Float.(
+               x >= left_bound && x <= right_bound && y >= left_bound
+               && y <= right_bound))
     in
     List.length intersections
   in
 
-  (* let rec loop hs =
-       match hs with
-       | a :: b :: c :: tl ->
-           solve_1 a b c |> ignore;
-           loop (b :: c :: tl)
-       | _ -> None
-     in
-
-     loop hails |> ignore;
-
-     let _ =
-       match hails with
-       | h0 :: h1 :: h2 :: _ -> solve_1 h0 h1 h2
-       | _ -> failwith "invalid input"
-     in *)
+  let _ =
+    let equations =
+      let rec aux acc hs =
+        match hs with
+        | hd :: next :: tl -> aux (make_equation hd next :: acc) tl
+        | _ -> acc
+      in
+      List.take hails 5 |> aux [] |> Array.of_list
+    in
+    match cramer equations with
+    | Some [ x; y; vx; vy ] ->
+        Stdio.printf "x: %f, y: %f, vx: %f, vy: %f\n" x y vx vy
+    | _ -> failwith "no solution"
+  in
   Utils.print_result (Int.to_string part1) ""
